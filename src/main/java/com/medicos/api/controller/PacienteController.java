f44b813739e69dcd7fcf610c798b0272f816db96
William Firmino <williams.an@hotmail.com>
2025-04-08 19:50:05 -0300

    Alterações antes do intervalo do dia 08/04/2025

--- src/main/java/com/medicos/api/controller/PacienteController.java
+++ src/main/java/com/medicos/api/controller/PacienteController.java
@@ -1,7 +1,10 @@
 package com.medicos.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -41,5 +44,17 @@ public class PacienteController {
     public void excluir(@PathVariable Integer id) {
         pacienteRepository.deleteById(id);
     }
+
+    // Listagem e Busca de pacientes
+
+    // Listagem de todos os pacientes (sem filtro, sem paginação)
+    @GetMapping("/listar")
+    public List<DadosListagemPacientes> listarPacientes() {
+        pacienteRepository.findAll();
+
+    }
+    // map() -> Mapeia (separa) os elementos de uma lista.
+    // stream() -> Faz com que consigamos utilizar métodos prontos.
+    // toList -> Transforma o resultado final em uma lista.
 
 }
